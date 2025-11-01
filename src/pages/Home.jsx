fe1a6c2baf4ce26cce09618e6d7c834bce3191c4
noymuljim <jim22205101671@diu.edu.bd>
2025-11-01 19:05:34 +0600

    removed issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import MyContainer from '../components/MyContainer';
-import HeroSection from '../components/HeroSection';
+
 
 const Home = () => {
     return (
         <MyContainer>
-            <div>
-                <HeroSection></HeroSection>
-            </div>
+            <section className='hero'>
+             <h1>hero</h1>
+            </section>
+
+
         </MyContainer>
     );
 };
